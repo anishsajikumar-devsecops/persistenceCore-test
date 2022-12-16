6c0b021f098402d234d79637f434e17157fd0387
betterclever <paliwal.pranjal83@gmail.com>
2022-12-16 12:20:51 +0400

    fix devnet setup

--- k8s/graph-node/scripts/init-firehose-node.sh
+++ k8s/graph-node/scripts/init-firehose-node.sh
@@ -1,52 +1,53 @@
 NODE_INDEX=${HOSTNAME##*-}
 echo "firehose-node Index: $NODE_INDEX"
 
+
+# print all environment variables
+printenv
+
+# If genesis json exists and force_init is not set, then skip the node initialization
+if [ "$FORCE_INIT" = "true" ]; then
+    echo "Force init is set to true. Initializing node."
+    rm -rf $HOME_DIR/*
+else
+    echo "Force init is set to false. Skipping node initialization."
+fi
+
+
 if test -f $HOME_DIR/config/genesis.json; then
     echo "Genesis.json exists. Skipping node initialization."
 else
     echo "Genesis.json does not exist. Initializing node."
 
-    NODE_NAME=$(jq -r ".firehose_nodes[$NODE_INDEX].name" /config-graph/firehose-node.json)
-    echo "firehose-node Index: $NODE_INDEX, Key name: $NODE_NAME"
+NODE_NAME=$(jq -r ".firehose_nodes[$NODE_INDEX].name" /config-graph/firehose-node.json)
+echo "firehose-node Index: $NODE_INDEX, Key name: $NODE_NAME"
 
-    echo "Printing genesis file before init"
-    ls -lrht $HOME_DIR/config
+# Print mnemonic
+MNEMONIC=$(jq -r ".firehose_nodes[$NODE_INDEX].mnemonic" /config-graph/firehose-node.json)
+echo "Mnemonic: $MNEMONIC"
 
-    jq -r ".firehose_nodes[$NODE_INDEX].mnemonic" /config-graph/firehose-node.json | persistenceCore init $NODE_NAME --chain-id $CHAIN_ID --home $HOME_DIR --recover
-    jq -r ".firehose_nodes[$NODE_INDEX].mnemonic" /config-graph/firehose-node.json | persistenceCore keys add $NODE_NAME --recover --keyring-backend="test" --home $HOME_DIR
+jq -r ".firehose_nodes[$NODE_INDEX].mnemonic" /config-graph/firehose-node.json | persistenceCore init $NODE_NAME --chain-id $CHAIN_ID --home $HOME_DIR --recover
+echo $MNEMONIC | persistenceCore keys add $NODE_NAME --recover --keyring-backend="test" --home $HOME_DIR
 
-    # if we have GENESIS_NODE_DATA_RESOLUTION_METHOD is dynamic fetch the genesis from the GENESIS_EXPOSED_PORT on the GENESIS_HOST 
+    # if we have GENESIS_NODE_DATA_RESOLUTION_METHOD is dynamic fetch the genesis from the GENESIS_EXPOSER_PORT on the GENESIS_HOST 
     # else fetch it directly from GENESIS_JSON_FETCH_URL
-    if [ "$GENESIS_NODE_DATA_RESOLUTION_METHOD" = "DYNAMIC" ]; then
-        echo "Config: DYNAMIC. Fetching genesis file from $GENESIS_HOST:$GENESIS_PORT"
-        GENESIS_NODE_ID=$(curl -s http://$GENESIS_HOST:$GENESIS_PORT/node_id)
-        curl http://$GENESIS_HOST:$GENESIS_PORT/genesis -o $HOME_DIR/config/genesis.json
+    GENESIS_NODE_ID=""
+    if [ $GENESIS_NODE_DATA_RESOLUTION_METHOD = "DYNAMIC" ]; then
+        echo "Config: DYNAMIC. Fetching genesis file from $GENESIS_HOST:$GENESIS_EXPOSER_PORT"
+        GENESIS_NODE_ID=$(curl -s http://$GENESIS_HOST:$GENESIS_EXPOSER_PORT/node_id)
+        curl http://$GENESIS_HOST:$GENESIS_EXPOSER_PORT/genesis -o $HOME_DIR/config/genesis.json
     else
         echo "Config: STATIC. Fetching genesis file from $GENESIS_JSON_FETCH_URL"
         curl $GENESIS_JSON_FETCH_URL -o $HOME_DIR/config/genesis.json
     fi
 
-    echo "Genesis file that we got....."
-    cat $HOME_DIR/config/genesis.json
-
-    GENESIS_NODE_P2P=$(curl -s http://$GENESIS_NODE_ID@$GENESIS_HOST:$GENESIS_PORT_P2P)
+    GENESIS_NODE_P2P="$GENESIS_NODE_ID@$GENESIS_HOST:$GENESIS_PORT_P2P"
     echo "Node P2P: $GENESIS_NODE_P2P"
     sed -i "s/persistent_peers = \"\"/persistent_peers = \"$GENESIS_NODE_P2P\"/g" $HOME_DIR/config/config.toml
     sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $HOME_DIR/config/config.toml
     sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $HOME_DIR/config/config.toml
     sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $HOME_DIR/config/config.toml
     sed -i 's/index_all_keys = false/index_all_keys = true/g' $HOME_DIR/config/config.toml
-
-    SNAP_RPC="https://rpc.testnet.persistence.one:443"
-
-    LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-    BLOCK_HEIGHT=$((LATEST_HEIGHT - 5000)); \
-    TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-    sed -i .bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-    s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-    s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-    s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME_DIR/config/config.toml
 
     # replace seeds if the variable is not empty
     if [ ! -z "$SEEDS" ]; then
