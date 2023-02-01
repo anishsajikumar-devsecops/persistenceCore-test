b27691f8ed43aa901597438f3d3d631a8de7375a
mkRuby009 <manasi@persistence.one>
2023-02-01 13:14:14 +0530

    v7 local chain upgrade with cosmovisor auto-download feature

--- contrib/local/upgrade.sh
+++ contrib/local/upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-OFFSET_HEIGHT=40
-UPGRADE_NAME=v3
+OFFSET_HEIGHT=100
+UPGRADE_NAME=v7
 
 set -o errexit -o nounset -o pipefail -eu
 
@@ -14,6 +14,7 @@ echo "Starting software upgrade"
 echo "### Submit proposal from val1"
 RESP=$($CHAIN_BIN tx gov submit-proposal software-upgrade $UPGRADE_NAME --yes --title "$UPGRADE_NAME" --description "$UPGRADE_NAME" \
     --upgrade-height $UPGRADE_HEIGHT --from val1 --chain-id $CHAIN_ID --deposit 100uxprt \
+    --upgrade-info "https://gist.githubusercontent.com/mkRuby009/f4165d18fbc2a57d248a5da1e1512777/raw/3ec2a9854aec90880ba6bf9f363142ca2ca297b2/v7_binaries.json" \
     --fees 2000uxprt --gas auto --gas-adjustment 1.5 -b block -o json)
 PROPOSAL_ID=$(echo "$RESP" | jq -r '.logs[0].events[] | select(.type == "submit_proposal") | .attributes[] | select(.key == "proposal_id") | .value')
 
